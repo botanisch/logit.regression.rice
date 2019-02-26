d0f7a888d592288b3a3e644171a0b7b67d9a2d0e
bongsongkim <43867065+bongsongkim@users.noreply.github.com>
2019-02-25 20:52:12 -0600

    Update (Figure3)classifications_of_minor_subpopulations.R

--- (Figure3)classifications_of_minor_subpopulations.R
+++ (Figure3)classifications_of_minor_subpopulations.R
@@ -38,16 +38,16 @@ phen2 <- dat3$Seed.number.per.panicle
 phen3 <- dat3$Florets.per.panicle
 phen4 <- dat3$Panicle.fertility 
 phen5 <- dat3$Straighthead.suseptability
-phen6 <- dat3$Blast.resistance    
-fit <- glm(Sub.population ~ phen1 + phen2 + phen3 + phen4 + phen5 + phen6, data=dat2,family=binomial(link = "logit"))
+phen6 <- dat3$Blast.resistance
+phen7 <- dat3$Protein.content
+fit <- glm(dat2$Sub.population ~ phen1 + phen2 + phen3 + phen4 + phen5 + phen6 + phen7,data=dat2,family=binomial(link = "logit"),control = list(maxit = 100))
 summary(fit) 
 
-
-logit <- function(x1,x2,x3,x4,x5,x6){       
-res <- 1/(1 + exp( -1* (coefficients(fit)[1] + x1*coefficients(fit)[2] + x2*coefficients(fit)[3] + x3*coefficients(fit)[4] + x4*coefficients(fit)[5] + x5*coefficients(fit)[6] + x6*coefficients(fit)[7] ))); return(res) 
+logit <- function(x1,x2,x3,x4,x5,x6,x7){       
+res <- 1/(1 + exp( -1* (coefficients(fit)[1] + x1*coefficients(fit)[2] + x2*coefficients(fit)[3] + x3*coefficients(fit)[4] + x4*coefficients(fit)[5] + x5*coefficients(fit)[6] + x6*coefficients(fit)[7] + x7*coefficients(fit)[8]))); return(res) 
        } 
 
-logit.res <- logit(phen1,phen2,phen3,phen4,phen5,phen6)
+logit.res <- logit(phen1,phen2,phen3,phen4,phen5,phen6,phen7)
 
 res.tab1 <- cbind(as.character(dat3[,4]),logit.res)[which(is.na(logit.res) == FALSE),]
 res.tab2 <- res.tab1[which(as.numeric(res.tab1[,2]) < .5),]
@@ -66,10 +66,17 @@ phen3 <- dat4$Florets.per.panicle
 phen4 <- dat4$Panicle.fertility 
 phen5 <- dat4$Straighthead.suseptability
 phen6 <- dat4$Blast.resistance    
+phen7 <- dat4$Protein.content
+fit <- glm(dat2$Sub.population ~ phen1 + phen2 + phen3 + phen4 + phen5 + phen6 + phen7,data=dat2,family=binomial(link = "logit"),control = list(maxit = 100))
+summary(fit) 
 
-logit.res <- logit(phen1,phen2,phen3,phen4,phen5,phen6)
+logit <- function(x1,x2,x3,x4,x5,x6,x7){       
+res <- 1/(1 + exp( -1* (coefficients(fit)[1] + x1*coefficients(fit)[2] + x2*coefficients(fit)[3] + x3*coefficients(fit)[4] + x4*coefficients(fit)[5] + x5*coefficients(fit)[6] + x6*coefficients(fit)[7] + x7*coefficients(fit)[8]))); return(res) 
+       } 
 
-res.tab1 <- cbind(as.character(dat4[,4]),logit.res)[which(is.na(logit.res) == FALSE),]
+logit.res <- logit(phen1,phen2,phen3,phen4,phen5,phen6,phen7)
+
+res.tab1 <- cbind(as.character(dat3[,4]),logit.res)[which(is.na(logit.res) == FALSE),]
 res.tab2 <- res.tab1[which(as.numeric(res.tab1[,2]) < .5),]
 res.tab3 <- res.tab1[which(as.numeric(res.tab1[,2]) > .5),]
 
@@ -86,14 +93,19 @@ phen3 <- dat5$Florets.per.panicle
 phen4 <- dat5$Panicle.fertility 
 phen5 <- dat5$Straighthead.suseptability
 phen6 <- dat5$Blast.resistance    
+phen7 <- dat5$Protein.content
+fit <- glm(dat2$Sub.population ~ phen1 + phen2 + phen3 + phen4 + phen5 + phen6 + phen7,data=dat2,family=binomial(link = "logit"),control = list(maxit = 100))
+summary(fit) 
 
-logit.res <- logit(phen1,phen2,phen3,phen4,phen5,phen6)
+logit <- function(x1,x2,x3,x4,x5,x6,x7){       
+res <- 1/(1 + exp( -1* (coefficients(fit)[1] + x1*coefficients(fit)[2] + x2*coefficients(fit)[3] + x3*coefficients(fit)[4] + x4*coefficients(fit)[5] + x5*coefficients(fit)[6] + x6*coefficients(fit)[7] + x7*coefficients(fit)[8]))); return(res) 
+       } 
 
-res.tab1 <- cbind(as.character(dat5[,4]),logit.res)[which(is.na(logit.res) == FALSE),]
+logit.res <- logit(phen1,phen2,phen3,phen4,phen5,phen6,phen7)
+
+res.tab1 <- cbind(as.character(dat3[,4]),logit.res)[which(is.na(logit.res) == FALSE),]
 res.tab2 <- res.tab1[which(as.numeric(res.tab1[,2]) < .5),]
 res.tab3 <- res.tab1[which(as.numeric(res.tab1[,2]) > .5),]
 
-plot(sort(res.tab1[,2]),col="red",pch=20,ylab="indica / japonica")
+plot(sort(as.numeric(res.tab1[,2])),col="red",pch=20,ylab="indica / japonica")
 abline(h=0.5)
-
-
